01bd5572c390054cde33a6004f37b5f7e3a82794
hashman8433 <843355856@qq.com>
2017-09-23 15:17:20 +0800

    add create table demo --20170923

---
+++ src/annotions/database/Uniqueness.java
@@ -0,0 +1,14 @@
+package annotions.database;
+
+/**
+ * ClassName:Uniqueness.java
+ * Reason:	 TODO ADD REASON
+ *
+ * @author   zhaozj
+ * @since    Ver 1.1
+ * @Date	 2017年9月23日
+ */
+public @interface Uniqueness {
+	Constraints constraints()
+		default @Constraints(unique = true);
+}
