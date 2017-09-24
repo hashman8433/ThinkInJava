1d34fc5b144611831804d8fb8d2c52ba9e2dade2
hashman8433 <843355856@qq.com>
2017-09-24 11:47:02 +0000

    add callable

--- src/concurrency/practice/FabonacciArray.java
+++ src/concurrency/practice/FabonacciArray.java
@@ -9,10 +9,10 @@ public class FabonacciArray implements Runnable {
 	//记录当前是第几个线程
 	private int id = count++;
 	
-	private int countTimes;
+	private int fabonacciNumber;
 	
-	public FabonacciArray(int times){
-		countTimes = times;
+	public FabonacciArray(int fabonacciNumber){
+		this.fabonacciNumber = fabonacciNumber;
 	}
 	//斐波那契数列：1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 	/**
@@ -21,14 +21,15 @@ public class FabonacciArray implements Runnable {
 	 * 	number[0] 第 n - 2 个数字
 	 * 	number[1] 第 n - 1 个数字
 	 * 	number[2] 第 n 个数字
-	 * @param countTimes 当前剩余计算次数
+	 * @param fabonacciNumber 当前剩余计算次数
+	 * @return TODO
 	 * @return
 	 */
-	public void FabonacciCount(int[] number, int times){
-		if(number.length < 3)  return;
+	public int fabonacciCaculate(int[] number, int times){
+		if(number.length < 3)  return 0;
 
 		//结束递归
-		if(times == 0)  return;
+		if(times == 0)  return 0;
 
 		int temp = number[1] + number[2];
 		number[0] = number[1];
@@ -36,9 +37,9 @@ public class FabonacciArray implements Runnable {
 		number[2] = temp;
 		
 		System.out.print(number[2] + " ");
-		FabonacciCount(number, --times);
+		fabonacciCaculate(number, --times);
 		
-		return;
+		return temp;
 	}
 	
 	@Override
@@ -46,15 +47,33 @@ public class FabonacciArray implements Runnable {
 		// TODO Auto-generated method stub
 		int[] number = {1, 1, 2};
 		System.out.print(number[0] + " " + number[1] + " ");
-		FabonacciCount(number, countTimes);
+		fabonacciCaculate(number, fabonacciNumber);
+		System.out.println();
 	}
 	
+	
 	public static void main(String[] args){
-		ExecutorService exec = Executors.newSingleThreadExecutor();
+		//SingleThread 单线程
+		ExecutorService singleExec = Executors.newSingleThreadExecutor();
+		for(int i = 0; i < 9; i++)
+			singleExec.execute(new FabonacciArray(i));
 		
-		exec.execute(new FabonacciArray(10));
+		singleExec.shutdown();
 		
-		exec.shutdown();
+		//FixedThreadPool 固定线程 在有限的线程内执行任务
+		ExecutorService fixedExec = Executors.newFixedThreadPool(3);
+		
+		for(int i = 0; i < 9; i++)
+			fixedExec.execute(new FabonacciArray(i));
+		fixedExec.shutdown();
+		
+		//CachedThreadPool 自动为每一个任务创建一个线程
+		ExecutorService cachedExec = Executors.newCachedThreadPool();
+		
+		for(int i = 0; i < 9; i++)
+			cachedExec.execute(new FabonacciArray(i));
+		cachedExec.shutdown();
+		
 	}
 
 }
