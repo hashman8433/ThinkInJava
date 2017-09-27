423e2fe33ad6e9e4aba37fbc6bf71306d0c190f4
hashman8433 <843355856@qq.com>
2017-09-27 18:01:48 +0800

    add ReponseUI

---
+++ src/concurrency/ExceptionThread.java
@@ -0,0 +1,24 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * ClassName:ExceptionThread.java
+ * Reason:	 TODO ADD REASON
+ *
+ * @author   zhaozj
+ * @since    Ver 1.1
+ * @Date	 2017年9月27日
+ */
+public class ExceptionThread implements Runnable {
+
+	@Override
+	public void run() {
+		throw new RuntimeException();
+	}
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		exec.execute(new ExceptionThread());
+	}
+}
---
+++ src/concurrency/ResponsiveUI.java
@@ -0,0 +1,39 @@
+package concurrency;
+
+import java.io.IOException;
+
+class UnresponsiveUI {
+	private volatile double d = 1;
+	public UnresponsiveUI() throws IOException {
+		while(d > 0)
+			d = d + (Math.PI + Math.E) / d;
+		System.in.read();
+	}
+}
+
+/**
+ * ClassName:ResponsiveUI.java
+ * Reason:	 TODO ADD REASON
+ *
+ * @author   zhaozj
+ * @since    Ver 1.1
+ * @Date	 2017年9月27日
+ */
+public class ResponsiveUI extends Thread {
+	private static volatile double d = 1;
+	public ResponsiveUI() {
+		setDaemon(true);
+		start();
+	}
+	public void run() {
+		while(true) {
+			d = d + (Math.PI + Math.E) / d;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		//! new UnresponsiveUI(); //Must kill this process
+		new ResponsiveUI();
+		System.in.read();
+		System.out.println(d); // Shows progress
+	}
+}
